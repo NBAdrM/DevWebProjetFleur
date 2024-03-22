0658f10c5b41270b8b6b95141d138a3524e80e56
Adrien Maestri <adrien.maestri@gmail.com>
2024-03-22 14:03:24 +0100

    Update connexion/inscription

--- connection.php
+++ connection.php
@@ -3,27 +3,96 @@
     include("footer.php");
 
     PrintHeader();
+    $nom="";
+    $email="";
+    $error="";
+
+    if(! empty($_GET["inscription"])) {
+        if(isset($_POST["nom"]) && isset($_POST["email"]) && isset($_POST["pwd"]) && isset($_POST["cpwd"])){
+            $nom = $_POST["nom"];
+            $email = $_POST["email"];
+            $pwd = $_POST["pwd"];
+            $cpwd = $_POST["cpwd"];
+            if($pwd != $cpwd){
+                $error="<h3 class=\"login-error\">Votre confirmation de mot de passe ne correspond pas</h3>";
+            }
+            else if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+                $error="<h3 class=\"login-error\">Votre email n'est pas valable</h3>";
+            }
+            else{
+                //TODO mettre les identifiant dans un fichier .conf et le retier du git
+                $mysqli = new mysqli("localhost", "root", "", "porsche",3308);
+                $result = $mysqli->query("SELECT * FROM user WHERE email=\"".$email."\"");
+                if($result->num_rows > 0){
+                    $error= "<h3 class=\"login-error\">Cette email est déjà asocier a un compte</h3>";
+                }
+                else{
+                    //TODO inserer dans la bdd avec un token aleatoire et enoyer l'email de confiramtion
+                }
+            }
+        }
+        
+        echo $error."
+        <div class=\"login-box inscription\">
+            <h2>Inscription</h2>
+            <form action=\"\" method=\"POST\">
+                <div class=\"user-box\">
+                    <input type=\"text\" name=\"nom\" required=\"\" value=\"".$nom."\"/>
+                    <label>Nom d'utilisateur</label>
+                </div>
+                <div class=\"user-box\">
+                    <input type=\"email\" name=\"email\" required=\"\" value=\"$email\"/>
+                    <label>Email</label>
+                </div>
+                <div class=\"user-box\">
+                    <input type=\"password\" name=\"pwd\" required=\"\"/>
+                    <label>Mot de passe</label>
+                </div>
+                <div class=\"user-box\">
+                    <input type=\"password\" name=\"cpwd\" required=\"\"/>
+                    <label>Confirmation Mot de passe</label>
+                </div>
+                <div class=\"switch-con\">
+                    <a href=\"connection.php\">
+                        Deja inscrit ?
+                    </a>
+                </div>
+                <button>
+                        SEND
+                    <span></span>
+                </button>
+            </form>
+        </div>";
+    }
+    else {
+        echo "<div class=\"login-box\">
+        <h2>Connection</h2>
+        <form action=\"\" method=\"POST\">
+            <div class=\"user-box\">
+                <input type=\"email\" name=\"email\" required=\"\"/>
+                <label>Email</label>
+            </div>
+            <div class=\"user-box\">
+                <input type=\"password\" name=\"pwd\" required=\"\"/>
+                <label>Mot de passe</label>
+            </div>
+            <div class=\"switch-con\">
+                <a href=\"connection.php?inscription=true\">
+                    Pas encore inscrit ?
+                </a>
+            </div>
+            <button>
+                    SEND
+                <span></span>
+            </button>
+        </form>
+    </div>
+";
+    }
 
 ?>
 
-    <div class="login-box">
     
-        <form>
-            <div class="user-box">
-                <input type="text" name="" required="">
-                <label>Username</label>
-            </div>
-            <div class="user-box">
-                <input type="password" name="" required="">
-                <label>Password</label>
-            </div>
-            <a href="#">
-                    SEND
-                <span></span>
-            </a><
-        </form>
-    </div>
-
 
 <?php
     
