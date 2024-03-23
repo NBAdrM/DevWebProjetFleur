59c3e184011f321612a40545c273976a7748560b
Sao <133856512+Saorex@users.noreply.github.com>
2024-03-23 14:50:32 +0100

    Test voiture avec BDD.php

--- voiture.php
+++ voiture.php
@@ -1,9 +1,30 @@
+<?php ob_start();
+$titre = "Catalogue de voiture";
+?>
+
 <?php
     include("header.php");
     include("footer.php");
 
     PrintHeader();
+    /* Connexion à une base MySQL */
+    $dsn = 'mysql:dbname=projet devweb;host=localhost';
+    $user = 'root';
+    $password = '';
 
+    try {
+        $pdo = new PDO($dsn, $user, $password);
+    } catch (PDOException $e) {
+        echo 'Connexion échoué : ' . $e->getMessage();
+    }
+
+    $req = "Select * FROM voiture";
+    $stmt = $pdo->prepare($req);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo"<pre>";
+    print_r($result);
     
 
     PrintFooter();
