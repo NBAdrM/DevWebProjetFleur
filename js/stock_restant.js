0df1eb4bbebf9f108dbcf77b39616db577e48581
Sao <julienhautecourt@gmail.com>
2024-03-26 15:25:43 +0100

    Affichage du stock onclick

--- js/stock_restant.js
+++ js/stock_restant.js
@@ -1,14 +1,22 @@
-document.addEventListener("click", function(){
+document.addEventListener('DOMContentLoaded', function(){
     const btnStock = document.querySelectorAll('.btn-stock');
 
     btnStock.forEach(btn =>{
         btn.addEventListener('click',function(){
+
+            const tdStock = btn.parentNode.previousElementSibling;
+            
+            if (tdStock.textContent==""){
         
-        const tdStock = btn.parentNode.previousElementSibling;
         const stock = parseInt(btn.dataset.stock);
         tdStock.textContent = stock;
 
+            }
+            
+            else {
+        tdStock.textContent = "";
+            }
+
         })
     })
-
 });
