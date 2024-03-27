7866de93d40faa53744bce5af4ecaf587c660ff3
GithubofKillian <killian.jonneaux@gmail.com>
2024-03-27 21:44:33 +0100

    css form (on peut changer si vous aimez pas)

--- contact.php
+++ contact.php
@@ -9,53 +9,51 @@
     <form action=\"\" method=\"GET\">
         <div class=\"form_contact\">
             <label for=\"nom\">Nom</label>
-            <input type=\"text\">
+            <input type=\"text\" placeholder=\"Nom*\">
         </div>
         <div class=\"form_contact\">
             <label for=\"nom\">Prénom</label>
-            <input type=\"text\">
+            <input type=\"text\" placeholder=\"Prénom*\">
         </div>
+        <br>
         <div class=\"form_contact\">
             <label>Genre</label>
             <input type=\"radio\" name=\"genre\">
-            <label for=\"nom\">Féminin </label>
+            <label for=\"nom\">Féminin</label>
             <input type=\"radio\" name=\"genre\">
-            <label for=\"nom\">Masculin </label>
+            <label for=\"nom\">Masculin</label>
         </div>
         <br>
         <div class=\"form_contact\">
             <label for=\"email\">Date de naissance</label>
             <input type=\"date\">
         </div>
-        <br>
         <div class=\"form_contact\">
             <label>Fonction</label>
                 <select>
                     <option value=\"\">--Veuillez choisir une option--</option>
-                    <option value=\"Enseignant\">Enseignant</option>
-                    <option value=\"Cadre\">Cadre</option>
                     <option value=\"Etudiant\">Etudiant</option>
-                    <option value=\"Retraité\">Retraité</option>
                     <option value=\"Sans emploi\">Sans emploi</option>
                     <option value=\"Technicien\">Technicien</option>
+                    <option value=\"Cadre\">Cadre</option>
+                    <option value=\"Retraité\">Retraité</option>
                     <option value=\"Autre\">Autre</option>
                 </select>
         </div>
-        <br>
         <div class=\"form_contact\">
             <label for=\"email\">Email</label>
-            <input type=\"email\">
+            <input type=\"email\" placeholder=\"Email*\">
         </div>
         <div class=\"form_contact\">
             <label for=\"nom\">Sujet</label>
-            <input type=\"text\">
+            <input type=\"text\" placeholder=\"Sujet*\">
         </div>
         <div class=\"form_contact\">
-            <label for=\"message\">Contenu</label>
-            <textarea rows=\"4\" cols=\"50\"></textarea>
+            <textarea rows=\"4\" cols=\"50\" placeholder=\"Contenu*\"></textarea>
         </div>
+        <br>
         <div class=\"form_contact\">
-            <input type=\"submit\" value=\"Envoyer\">
+            <span type=\"submit\">Envoyer</span>
         </div>
     </form>";
 ?> 
