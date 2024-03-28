6dbf1f33572163841e60cbe4aa1c7c4181b4bade
Adrien Maestri <adrien.maestri@gmail.com>
2024-03-28 11:24:12 +0100

    Le panier

--- js/compteur.js
+++ js/compteur.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
       const decreBtn = compteur.querySelector('.btn-decre');
       const increBtn = compteur.querySelector('.btn-incre');
       const quantite = compteur.querySelector('.quantite');
+      const quantite2 = compteur.querySelector('.quantite2');
       const stock = parseInt(compteur.dataset.stock);
   
       let count = 0;
@@ -12,13 +13,32 @@ document.addEventListener('DOMContentLoaded', function() {
       decreBtn.addEventListener('click', function() {
         count = Math.max(0, count - 1);
         quantite.textContent = count;
+        quantite2.value = count;
+        if(count==0){
+          quantite2.disabled = true;
+        }
       });
   
       increBtn.addEventListener('click', function() {
         if (count < stock){
         count++;
         quantite.textContent = count;
+        quantite2.value = count;
+        if (quantite2.disabled) {
+          quantite2.disabled=false;
+        }
+        updateCounterState(compteur);
         }
       });
+      
     });
+      
+    function updateCounterState(currentCounter) {
+      cpt.forEach(compteur => {
+          if (compteur !== currentCounter) {
+            const quantite2 = compteur.querySelector('.quantite2');
+            quantite2.disabled = true;
+          }
+      });
+    }
   });
--- voiture.php
+++ voiture.php
@@ -2,43 +2,60 @@
     include("include/header.php");
     include("include/footer.php");
     include("include/fonction.php");
-    //TODO Faire afficher le stock que quand on clic sur le mot stock
+
     PrintHeader(); 
-    $req = requetBDD("SELECT * FROM voiture");
+
+    $panier = '';
+    if(isset($_POST['voiture'])){
+      if(empty($_SESSION['id'])){
+        header('Location: connection.php');
+      }
+      else{
+        $result = requetBDD("INSERT INTO panier (id_user,id_ref,nb) VALUE ('".$_SESSION['id']."','".$_POST['voiture']."','".$_POST['quantite']."')");
+        if ($result) {
+          $panier = "<h2 class=\"conf_panier\">L'article a été ajouter a votre panier</h2>";
+        }
+        else{
+          $panier = "<h2 class=\"conf_panier\">Un probleme est survenue</h2>";
+        }
+      }
+    }
+    $req = requetBDD("SELECT * FROM marchandise");
 ?>
 
 <script src="js/compteur.js"></script>
-<script src="js/stock_restant.js"></script>
-
-<table class="voiture">  
-  <tbody>
-    <?php foreach ($req as $v) : ?>
-      <tr>
-        <td>
-          <a href="./img/<?= $v['image_path'] ?>" target="_blank">
-            <figure>
-              <img src="./img/<?= $v['image_path'] ?>" alt="<?= $v['nom']?>">
-              <figcaption> <?= $v['nom']?> </figcaption>
-            </figure> 
-          </a>
-        </td>
-        <td><?= $v['prix'] ?>€</td>
-        <td></td> 
-        <td>
-          <div class="compteur" data-stock="<?= $v['stock'] ?>">
-            <button class="btn-decre">-</button>
-            <button class="btn-incre">+</button>
-              <div class="quantite">0</div>
-          </div>
-          <button type="submit" class="btn">
-            <span class="btn__visible">Ajouter au panier</span>
-            <span class="btn__invisible"><?= $v['stock'] ?> Restant</span>
-          </button>
-        </td>
-      </tr>  
-    <?php endforeach; ?>
-  </tbody>
-</table>
+<?= $panier ?>
+<form action="" method="POST">
+  <table class="voiture">  
+    <tbody>
+      <?php foreach ($req as $v) : ?>
+        <tr>
+          <td>
+            <a href="./img/<?= $v['image_path'] ?>" target="_blank">
+              <figure>
+                <img src="./img/<?= $v['image_path'] ?>" alt="<?= $v['nom']?>">
+                <figcaption> <?= $v['nom']?> </figcaption>
+              </figure> 
+            </a>
+          </td>
+          <td><?= $v['prix'] ?>€</td>
+          <td></td> 
+          <td>
+            <div class="compteur" data-stock="<?= $v['stock'] ?>">
+              <button type="button" class="btn-decre">-</button>
+              <button type="button" class="btn-incre">+</button>
+                <div class="quantite">0</div>
+                <input class="quantite2" name="quantite" type="number" value="0" disabled/>
+            </div>
+            <button name="voiture" type="submit" class="btn" value="<?= $v['id'] ?>">
+              <span class="btn__visible">Ajouter au panier</span>
+              <span class="btn__invisible"><?= $v['stock'] ?> Restant</span>
+            </button>
+          </td>
+        </tr>  
+      <?php endforeach; ?>
+    </tbody>
+  </table>
 
 
 
